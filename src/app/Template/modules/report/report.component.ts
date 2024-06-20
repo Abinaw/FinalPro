052926a1da7c515c2bb1351e7bf95d697a8a9e3f
kabil19 <kabilankrishna5@gmail.com>
2024-06-20 13:47:26 +0530

    [20/06/2024]

--- src/app/Template/modules/report/report.component.ts
+++ src/app/Template/modules/report/report.component.ts
@@ -7,4 +7,52 @@ import { Component } from '@angular/core';
 })
 export class ReportComponent {
 
+  
+    Cards = [
+        {
+            name: " Stock Report ",
+            route: "/dash-board/customer",
+        },
+        {
+            name: "Sales Report",
+            route: "/dash-board/user",
+        },
+        {
+            name: "Purchase Report",
+            route: "/dash-board/vendor",
+        },
+        {
+            name: "Payments Report",
+            route: "/dash-board/commonPayments",
+        },
+        {
+            name: "Vendor Report",
+            route: "/dash-board/return",
+        },
+        // {
+        //     name: "Stock",
+        //     imageUrl: "../../assets/New Set/stock.png",
+        //     route: "/dash-board/stock",
+        // },
+        // {
+        //     name: "Purchase",
+        //     imageUrl: "../../assets/New Set/purchase.png",
+        //     route: "/dash-board/purchase",
+        // },
+        // {
+        //     name: "Category",
+        //     imageUrl: "../../assets/New Set/category.png",
+        //     route: "/dash-board/category",
+        // },
+        // {
+        //     name: "Invoice",
+        //     imageUrl: "../../assets/New Set/invoice.png",
+        //     route: "/dash-board/invoice",
+        // },
+        // {
+        //     name: "Report",
+        //     imageUrl: "../../assets/New Set/report.png",
+        //     route: "/dash-board/report",
+        // },
+    ];
 }

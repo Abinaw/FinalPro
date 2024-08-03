f2e9587971583cdfc302229d64175d8487e2dbc7
Abinaw <dabinawan@gmail.com>
2024-08-03 17:10:44 +0530

    dialog changes are made

--- src/app/Template/side-bar/nav-settings/notification/notification.component.ts
+++ src/app/Template/side-bar/nav-settings/notification/notification.component.ts
@@ -10,39 +10,39 @@ import { INotificationEntity } from 'src/app/constants/interfaces/INotificationE
   templateUrl: './notification.component.html',
   styleUrls: ['./notification.component.css']
 })
-export class NotificationComponent  implements OnChanges  {
-    
-    
+export class NotificationComponent implements OnChanges {
 
-    @Input('notificationData') notificationData: INotificationEntity | null | undefined
 
-    salesChequeDuesCount: number = 0;
-    purchaseChequeDuesCount: number = 0;
-    productsLowerThanReorderLevelCount :number = 0;  
-    constructor(public matDialog: MatDialog) {}
-  
-    ngOnChanges(changes: SimpleChanges): void {
-        if (changes['notificationData']) {
-          this.updateCounts();
-        }
-      }
-    
-      private updateCounts(): void {
-        if (this.notificationData) {
-          this.salesChequeDuesCount = this.notificationData.salesChequeDues?.length || 0;
-          this.purchaseChequeDuesCount = this.notificationData.purchaseChequeDues?.length || 0;
-          this.productsLowerThanReorderLevelCount =  this.notificationData.productsLowerThanReorderLevel?.length || 0;
-        } else {
-          this.salesChequeDuesCount = 0;
-          this.purchaseChequeDuesCount = 0;
-          this.productsLowerThanReorderLevelCount = 0;
-        }
-      }
 
-    
-    openNotification() {
-        console.log(this.notificationData)
-      this.matDialog.open(NotifiExpansionComponent, { data: this.notificationData, panelClass: "custom-notification-dialog-container",height:"30%", width:"auto" });
+  @Input('notificationData') notificationData: INotificationEntity | null | undefined
+
+  salesChequeDuesCount: number = 0;
+  purchaseChequeDuesCount: number = 0;
+  productsLowerThanReorderLevelCount: number = 0;
+  constructor(public matDialog: MatDialog) { }
+
+  ngOnChanges(changes: SimpleChanges): void {
+    if (changes['notificationData']) {
+      this.updateCounts();
     }
-    
   }
+
+  private updateCounts(): void {
+    if (this.notificationData) {
+      this.salesChequeDuesCount = this.notificationData.salesChequeDues?.length || 0;
+      this.purchaseChequeDuesCount = this.notificationData.purchaseChequeDues?.length || 0;
+      this.productsLowerThanReorderLevelCount = this.notificationData.productsLowerThanReorderLevel?.length || 0;
+    } else {
+      this.salesChequeDuesCount = 0;
+      this.purchaseChequeDuesCount = 0;
+      this.productsLowerThanReorderLevelCount = 0;
+    }
+  }
+
+
+  openNotification() {
+    console.log(this.notificationData)
+    this.matDialog.open(NotifiExpansionComponent, { data: this.notificationData, panelClass: "custom-notification-dialog-container", height: "30%", width: "auto", backdropClass: "notification-backdrop" });
+  }
+
+}
